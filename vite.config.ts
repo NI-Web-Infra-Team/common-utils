d939cd66f23c8bdeca2d603cc10960d8a888786c
liu <liunian7965@navinfo.com>
2023-05-20 12:22:27 +0800

    chore: 修改 coverage 命令

--- vite.config.ts
+++ vite.config.ts
@@ -35,4 +35,10 @@ export default defineConfig({
       content: bannerContent,
     }),
   ],
+  test: {
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+    },
+  },
 });
