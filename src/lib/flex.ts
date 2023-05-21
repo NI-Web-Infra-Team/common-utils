f0a14eb0bfdfff341a56a719d3a447f9d1ba40f0
liu <liunian7965@navinfo.com>
2023-05-22 07:33:11 +0800

    fix: 修复错误的参数拼写

--- src/lib/flex.ts
+++ src/lib/flex.ts
@@ -2,7 +2,7 @@
  * 获取屏幕缩放比例
  * @function getScreenScaleSize
  *
- * @param {number} defautSize 默认尺寸
+ * @param {number} defaultSize 默认尺寸
  * @param {number | undefined} fractionDigits 小数位数
  * @returns {number} 屏幕缩放比例
  *
@@ -10,5 +10,5 @@
  * const screenScaleSize = getScreenScaleSize(1920, 2);
  * console.log('屏幕缩放比例', screenScaleSize);
  */
-export const getScreenScaleSize = (defautSize?: number, fractionDigits?: number | undefined): number =>
-  Number((globalThis.document.documentElement.clientWidth / (defautSize || 1920)).toFixed(fractionDigits ?? 3));
+export const getScreenScaleSize = (defaultSize?: number, fractionDigits?: number | undefined): number =>
+  Number((globalThis.document.documentElement.clientWidth / (defaultSize || 1920)).toFixed(fractionDigits ?? 3));
