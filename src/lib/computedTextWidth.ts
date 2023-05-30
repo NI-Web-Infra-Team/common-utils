4198e2ec1f3d6f8dca329e9b8a81808234a9e019
liu <liunian7965@navinfo.com>
2023-05-30 15:59:07 +0800

    refactor: 修改 computedTextWidth 返回值说明

--- src/lib/computedTextWidth.ts
+++ src/lib/computedTextWidth.ts
@@ -4,7 +4,7 @@ let canvas: HTMLCanvasElement;
  * 计算文本宽度
  * @param {string} text - 文本
  * @param {string} font - 字体
- * @returns {number} - 宽度
+ * @returns {number} - 宽度，单位 px
  */
 export function computedTextWidth(text: string, font: string) {
   canvas = canvas ?? document.createElement("canvas");
