2904fe0338a0cf2002ab13443a7b3e9dd08babcf
liunian <liunian@2077tech.com>
2023-07-05 17:43:42 +0800

    feat: 新增获取元素样式的值的函数

---
+++ tests/e2e/lib/getStylePropertyValue.spec.ts
@@ -0,0 +1,13 @@
+import { getStylePropertyValue } from "../../../src/lib/getStylePropertyValue";
+
+describe("getStylePropertyValue", () => {
+  it("获取元素样式的值", () => {
+    const targetSize = "16px";
+    cy.get("body")
+      .invoke("css", "font-size", targetSize)
+      .then((el) => {
+        const body = el[0] as unknown as HTMLBodyElement;
+        expect(body.style.fontSize).to.be.equal(getStylePropertyValue(body, "font-size"));
+      });
+  });
+});
